427f038855048e94367c9eb4954c70341fa42e87
green-study <soo010410@naver.com>
2022-11-06 18:39:50 +0900

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyDFKnQdwkafQyHVGbEFs-LVDT6QdCA7S1A",
+authDomain: "sw-engineering-9a5bb.firebaseapp.com",
+projectId: "sw-engineering-9a5bb",
+storageBucket: "sw-engineering-9a5bb.appspot.com",
+messagingSenderId: "971505813449",
+appId: "1:971505813449:web:5f8031296abcd5e6b10a18"
+};
+
