0cd12ad5ab2563f7f962d63cf409f1e5f3de0d44
subin1509 <subin1509@naver.com>
2022-11-08 16:32:29 +0900

    login check & comment box

--- exerciseDisorder/exerciseDisorder.js
+++ exerciseDisorder/exerciseDisorder.js
@@ -1,5 +1,9 @@
 function try_page() {
-    location.href="./bookselect.html"
+    if(localStorage.getItem("user")===null){
+        alert("체험을 위해서는 로그인이 필요합니다.");
+    }else{
+        location.href="./bookselect.html";
+    }
 }
 
 function end_page() {
